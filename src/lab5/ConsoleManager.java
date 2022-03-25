1d024cfa79fd93c3b0a62e0e1f81f78729171fbe
xGodness <alexei.gotovko@gmail.com>
2022-03-25 20:46:16 +0300

    upd 4

--- src/lab5/ConsoleManager.java
+++ src/lab5/ConsoleManager.java
@@ -1,16 +1,19 @@
 package lab5;
 
+import lab5.commands.Command;
+
+import java.util.HashMap;
+
 public class ConsoleManager {
 
+    private IOManager ioManager;
     private boolean isActive = true;
-
-
+    private HashMap<String, Command> commandsHashMap;
 
     public void execute() {
         while (isActive) {
-            System.out.print("Type command (type \"help\" for help): ");
+            String input = ioManager.getNextInput("Type command (type \"help\" for help): ");
+
         }
     }
 
-
-}
