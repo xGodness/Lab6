7f57b225faa71305429740dac3b688433eff986b
xGodness <alexei.gotovko@gmail.com>
2022-05-07 03:51:20 +0300

    upd 7: added logging (kinda)

--- src/lab6/commands/CommandImpl.java
+++
@@ -1,9 +0,0 @@
-package lab6.commands;
-
-import lab6.exceptions.collectionexceptions.CollectionException;
-import com.sun.istack.internal.NotNull;
-import lab6.collection.CollectionManagerImpl;
-
-public interface CommandImpl {
-    String execute(@NotNull CollectionManagerImpl moviesCollection, Object[] args) throws CollectionException;
-}
